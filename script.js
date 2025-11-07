717d63e323d3f51b45d941f3df44f23c8f63d6ad
asim16111980 <asim16111980@gmail.com>
2025-11-07 21:18:10 +0200

    Create Clock Project

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get references for elements
+const secondHand = document.getElementsByClassName("second");
+const minuteHand = document.getElementsByClassName("minute");
+const hourHand = document.getElementsByClassName("hour");
+const modeSwitch = document.getElementsByClassName("mode-switch");
+
+// 
